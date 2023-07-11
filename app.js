41eaaa12855135df9efc6eb6be92d49cff11dec8
Abhimanyu Yadav <yadavabhimanyu084@gmail.com>
2023-07-11 16:55:48 +0530

    All required files, and ready to work project.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const heading = React.createElement("h1", { id: "heading" }, "Hello world from react!!!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom/client";
+//const heading = React.createElement("h1", { id: "heading" }, "Hello world from react!!!");
 const parent = React.createElement("div", { id: "parent" },
     React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "Nested h1 tag"),
-        React.createElement("h2", {}, "Nested h2 tag"),
+        React.createElement("h1", {id: "child1-1"}, "Nested h1 tag change it"),
+        React.createElement("h2", {id: "child1-2"}, "Nested h2 tag change it"),
     ]),
     React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "Nested h1 tag"),
-        React.createElement("h2", {}, "Nested h2 tag"),
+        React.createElement("h1", {id: "child2-1"}, "Nested h1 tag, change it"),
+        React.createElement("h2", {id: "child2-2"}, "Nested h2 tag"),
     ]));
-root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent); 
